5cb43b9e947328ecb4abf6d4cc1049c4ca586150
DiegoO2 <diego.hidalgo23@student.cart.org>
2024-02-15 15:49:31 -0800

    js

--- 3-everything.js
+++ 3-everything.js
@@ -1,23 +1,30 @@
 function every(array, test) {
-    for(item of array){
-      if(test(item)){
-        
-      }
-      else{
-        return false;
-      }
+  for (item of array) {
+    if (test(item)) {
+
     }
-    return true;
+    else {
+      return false;
+    }
   }
-  
-  function everySomeMethod(){
+  return true;
+}
 
-  }
+function everySomeMethod(array, test) {
+  return array.some(test);
+}
 
 
-  console.log(every([1, 3, 5], n => n < 10));
-  // → true
-  console.log(every([2, 4, 16], n => n < 10));
-  // → false
-  console.log(every([], n => n < 10));
-  // → true
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
+
+console.log(everySomeMethod([1, 3, 5], n => n < 10));
+// → true
+console.log(everySomeMethod([2, 4, 16], n => n < 10));
+// → false
+console.log(everySomeMethod([], n => n < 10));
+// → true
--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,10 +1,35 @@
+const SCRIPTS = require('./includes/scripts.js');
+
 require('./includes/scripts.js') // include the scripts used by the chapter
 
+// console.log(characterScript());
 function dominantDirection(text) {
-    // Your code here.
+  let textArray = [];
+  for (i = 0; i < text.length; i++) {
+    textArray.push(text.codePointAt(i));
   }
-  
-  console.log(dominantDirection("Hello!"));
-  // → ltr
-  console.log(dominantDirection("Hey, مساء الخير"));
-  // → rtl
+  let directionArray = [];
+  for (i of textArray) {
+    for (let script of SCRIPTS) {
+      if (script.ranges.some(([from, to]) => {
+        return i >= from && i < to;
+      })) {
+        directionArray.push(script.direction);
+      }
+    }
+  }
+  return directionArray
+
+
+  // for(script of SCRIPTS){
+
+  // }
+
+}
+
+
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
